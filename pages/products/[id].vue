ac8431a13de08b8266813adfbc05d316d6877ea4
Shourov Roy <shourovroy2389@gmail.com>
2023-04-17 00:02:21 +0600

    init commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Product</h1>
+    <p>{{ id }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const id = ref(route.params.id);
+throw createError("Product not found");
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtClientFallback>
+      <h1>Products</h1>
+      <NuxtPage />
+      <template #fallback="{ error }">
+        You can display the error locally here.
+      </template>
+    </NuxtClientFallback>
+  </div>
+</template>
